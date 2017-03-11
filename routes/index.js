b8332e584ec2b9cf3d59d18d8fcbf81a0152a608
Jayant Arora <jaistudy1996@gmail.com>
2017-03-11 18:46:05 -0500

    setup request module and test contribution link

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	request('https://github.com/users/jaistudy1996/contributions', function(error, response, body){
+		console.log(error);
+		console.log(response);
+		console.log(body);
+	});
   res.render('index', { title: 'Express' });
 });
 
