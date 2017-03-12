643257d94ecb498e671d380772f1f5054291f5cd
Jayant Arora <jaistudy1996@gmail.com>
2017-03-11 19:31:51 -0500

    Add node-htmlparser to parser the recieved html. Test-1 done

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
+var htmlParser = require('htmlparser');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	request('https://github.com/users/jaistudy1996/contributions', function(error, response, body){
-		console.log(error);
-		console.log(response);
-		console.log(body);
+		if(error){
+			console.log(error);
+		}
+		// console.log(response);
+		var handler = new htmlParser.DefaultHandler(function(error, dom){
+			if(error){
+				console.log(error);
+			}
+			else{
+				return dom;
+			}
+		});
+		var parser = new htmlParser.Parser(handler);
+		parser.parseComplete(body);
+		console.log(handler);
+		console.log("============");
+		console.log(parser);
+		// console.log(body);
 	});
   res.render('index', { title: 'Express' });
 });
